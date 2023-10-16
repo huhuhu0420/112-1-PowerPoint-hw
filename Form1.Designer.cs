58dffe986bb06df04f335046a4509d03733fe349
huhuhu0420 <ghostcheng.00@gmail.com>
2023-10-16 11:33:05 +0800

    tool strip

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this._delete = new System.Windows.Forms.DataGridViewButtonColumn();
-            this._shapeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this._information = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.shapeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.information = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.InsertButton = new System.Windows.Forms.Button();
@@ -39,8 +40,13 @@
             this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -48,50 +54,50 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this._delete, this._shapeName, this._information });
-            this.dataGridView1.Location = new System.Drawing.Point(666, 68);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.delete, this.shapeName, this.information });
+            this.dataGridView1.Location = new System.Drawing.Point(666, 90);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(254, 429);
+            this.dataGridView1.Size = new System.Drawing.Size(254, 407);
             this.dataGridView1.TabIndex = 0;
             // 
             // delete
             // 
-            this._delete.HeaderText = "刪除";
-            this._delete.MinimumWidth = 6;
-            this._delete.Name = "delete";
-            this._delete.ReadOnly = true;
-            this._delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this._delete.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this._delete.Text = "刪除";
-            this._delete.UseColumnTextForButtonValue = true;
-            this._delete.Width = 65;
+            this.delete.HeaderText = "刪除";
+            this.delete.MinimumWidth = 6;
+            this.delete.Name = "delete";
+            this.delete.ReadOnly = true;
+            this.delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.delete.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.delete.Text = "刪除";
+            this.delete.UseColumnTextForButtonValue = true;
+            this.delete.Width = 65;
             // 
             // shapeName
             // 
-            this._shapeName.DataPropertyName = "ShapeName";
-            this._shapeName.HeaderText = "形狀";
-            this._shapeName.MinimumWidth = 6;
-            this._shapeName.Name = "shapeName";
-            this._shapeName.ReadOnly = true;
-            this._shapeName.Width = 65;
+            this.shapeName.DataPropertyName = "ShapeName";
+            this.shapeName.HeaderText = "形狀";
+            this.shapeName.MinimumWidth = 6;
+            this.shapeName.Name = "shapeName";
+            this.shapeName.ReadOnly = true;
+            this.shapeName.Width = 65;
             // 
             // information
             // 
-            this._information.DataPropertyName = "Info";
-            this._information.HeaderText = "資訊";
-            this._information.MinimumWidth = 6;
-            this._information.Name = "information";
-            this._information.ReadOnly = true;
-            this._information.Width = 125;
+            this.information.DataPropertyName = "Info";
+            this.information.HeaderText = "資訊";
+            this.information.MinimumWidth = 6;
+            this.information.Name = "information";
+            this.information.ReadOnly = true;
+            this.information.Width = 125;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 32);
+            this.button1.Location = new System.Drawing.Point(12, 53);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(148, 107);
@@ -102,16 +108,16 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] { "線", "矩形" });
-            this.comboBox1.Location = new System.Drawing.Point(780, 32);
+            this.comboBox1.Location = new System.Drawing.Point(788, 56);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(110, 28);
+            this.comboBox1.Size = new System.Drawing.Size(110, 23);
             this.comboBox1.TabIndex = 3;
             // 
             // InsertButton
             // 
-            this.InsertButton.Font = new System.Drawing.Font("Noto Sans", 7.8F);
-            this.InsertButton.Location = new System.Drawing.Point(685, 31);
+            this.InsertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.InsertButton.Location = new System.Drawing.Point(685, 53);
             this.InsertButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.InsertButton.Name = "InsertButton";
             this.InsertButton.Size = new System.Drawing.Size(55, 29);
@@ -124,13 +130,13 @@
             // 
             this.InformationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.AboutToolStripMenuItem });
             this.InformationToolStripMenuItem.Name = "InformationToolStripMenuItem";
-            this.InformationToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.InformationToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.InformationToolStripMenuItem.Text = "說明";
             // 
             // AboutToolStripMenuItem
             // 
             this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
-            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
             this.AboutToolStripMenuItem.Text = "關於";
             // 
             // menuStrip1
@@ -140,31 +146,68 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(932, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(932, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 147);
+            this.button2.Location = new System.Drawing.Point(12, 168);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(148, 107);
             this.button2.TabIndex = 5;
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.toolStripButton3, this.toolStripButton2, this.toolStripButton1 });
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(932, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "○";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "⬜";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "📏";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(932, 510);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.InsertButton);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
-            this.Font = new System.Drawing.Font("Noto Sans", 8.999999F);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F);
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
@@ -172,9 +215,19 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
 
         private System.Windows.Forms.Button button2;
 
@@ -191,11 +244,12 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
 
+        private System.Windows.Forms.DataGridViewButtonColumn delete;
+        
+        private System.Windows.Forms.DataGridViewTextBoxColumn shapeName;
+        
+        private System.Windows.Forms.DataGridViewTextBoxColumn information;
         #endregion
-
-        private System.Windows.Forms.DataGridViewButtonColumn _delete;
-        private System.Windows.Forms.DataGridViewTextBoxColumn _shapeName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn _information;
         
         private Model _model = new Model();
     }
