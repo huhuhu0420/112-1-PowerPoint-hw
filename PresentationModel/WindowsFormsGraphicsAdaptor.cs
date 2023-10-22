5d14c5705e799fef53cdb25d1eb28e696ef93e45
huhuhu0420 <ghostcheng.00@gmail.com>
2023-10-22 18:31:27 +0800

    draw rect any dir

--- PresentationModel/WindowsFormsGraphicsAdaptor.cs
+++ PresentationModel/WindowsFormsGraphicsAdaptor.cs
@@ -16,6 +16,28 @@ namespace PowerPoint.PresentationModel
         public void ClearAll()
         {
         }
+
+        /// <summary>
+        /// rect
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        public void HandleRectangle(Point point1, Point point2)
+        {
+            float width = System.Math.Abs(point2.X - point1.X);
+            float height = System.Math.Abs(point2.Y - point1.Y);
+            float fix1 = point1.X;
+            float fix2 = point1.Y;
+            if (point1.X > point2.X)
+            {
+                fix1 -= width;
+            }
+            if (point1.Y > point2.Y)
+            {
+                fix2 -= height;
+            }
+            _graphics.DrawRectangle(Pens.DodgerBlue, fix1, fix2, width, height);
+        }
         
         /// <summary>
         /// draw
@@ -32,8 +54,7 @@ namespace PowerPoint.PresentationModel
                         (float)point2.Y);
                     break;
                 case ShapeType.RECTANGLE:
-                    _graphics.DrawRectangle(Pens.DodgerBlue, (float)point1.X, (float)point1.Y, (float)(point2.X - point1.X),
-                    (float)(point2.Y - point1.Y));
+                    HandleRectangle(point1, point2);
                     break;
                 case ShapeType.CIRCLE:
                     _graphics.DrawEllipse(Pens.DodgerBlue, (float)point1.X, (float)point1.Y, (float)(point2.X - point1.X),
