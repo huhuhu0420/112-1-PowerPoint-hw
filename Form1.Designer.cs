169e096febba29448bd2519bb8f9c2bfe55318b2
huhuhu0420 <ghostcheng.00@gmail.com>
2023-11-09 10:32:54 +0800

    rm btn 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,7 +41,6 @@ namespace PowerPoint
             this.InformationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.button2 = new System.Windows.Forms.Button();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.lineButton = new System.Windows.Forms.ToolStripButton();
             this.squareButton = new System.Windows.Forms.ToolStripButton();
@@ -154,15 +153,6 @@ namespace PowerPoint
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 168);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 107);
-            this.button2.TabIndex = 5;
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // toolStrip1
             // 
             this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
@@ -220,7 +210,6 @@ namespace PowerPoint
             this.ClientSize = new System.Drawing.Size(932, 510);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.InsertButton);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
@@ -251,8 +240,6 @@ namespace PowerPoint
         private System.Windows.Forms.ToolStripButton circleButton;
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-
-        private System.Windows.Forms.Button button2;
 
         private System.Windows.Forms.Button InsertButton;
 
