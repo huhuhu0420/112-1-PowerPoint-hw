b8e4487b872dba65ba60b7e3540970821b455ab3
huhuhu0420 <ghostcheng.00@gmail.com>
2023-11-18 20:26:21 +0800

    draw select point

--- PresentationModel/WindowsFormsGraphicsAdaptor.cs
+++ PresentationModel/WindowsFormsGraphicsAdaptor.cs
@@ -61,6 +61,22 @@ namespace PowerPoint.PresentationModel
             _graphics.DrawEllipse(pen, (float)point1.X, (float)point1.Y, (float)(point2.X - point1.X),
             (float)(point2.Y - point1.Y));
         }
+        
+        public void DrawSelectPoint(Pen pen, Point point1, Point point2)
+        {
+            const int radius = 3;
+            const int diameter = radius * 2;
+            _graphics.DrawEllipse(pen, point1.X - radius, point1.Y - radius, diameter, diameter);
+            _graphics.DrawEllipse(pen, point2.X - radius, point2.Y - radius, diameter, diameter);
+            _graphics.DrawEllipse(pen, point1.X - radius, point2.Y - radius, diameter, diameter);
+            _graphics.DrawEllipse(pen, point2.X - radius, point1.Y - radius, diameter, diameter);
+            int fix1 = (point1.X + point2.X) / 2;
+            int fix2 = (point1.Y + point2.Y) / 2;
+            _graphics.DrawEllipse(pen, fix1 - radius, point1.Y - radius, diameter, diameter);
+            _graphics.DrawEllipse(pen, fix1 - radius, point2.Y - radius, diameter, diameter);
+            _graphics.DrawEllipse(pen, point1.X - radius, fix2 - radius, diameter, diameter);
+            _graphics.DrawEllipse(pen, point2.X - radius, fix2 - radius, diameter, diameter);
+        }
 
         /// <summary>
         /// draw
@@ -82,6 +98,8 @@ namespace PowerPoint.PresentationModel
                 fix2 -= height;
             }
             _graphics.DrawRectangle(pen, fix1, fix2, width, height);
+            Pen pointPen = new Pen(Color.DeepPink, 1);
+            DrawSelectPoint(pointPen, point1, point2);
         }
     }
 }
