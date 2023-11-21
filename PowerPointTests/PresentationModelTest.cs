4a97dccd792acb4f344bb110d574c86cb8181fc9
huhuhu0420 <ghostcheng.00@gmail.com>
2023-11-21 11:31:14 +0800

    presentation model test

--- PowerPointTests/PresentationModelTest.cs
+++ PowerPointTests/PresentationModelTest.cs
@@ -1,27 +1,100 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PowerPoint;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Moq;
 using System.Drawing;
+using System.Windows.Forms;
 using PowerPoint.State;
 
-namespace PowerPoint.Tests
+namespace PowerPoint.PresentationModel.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class PresentationModelTests
     {
-        private PresentationModel.PresentationModel _presentationModel;
-        private PrivateObject _privatePresentationModel;
-        
-        [TestInitialize()]
-        public void Initialize()
+        private Mock<Model> _mockModel;
+        private PresentationModel _presentationModel;
+
+        [TestInitialize]
+        public void Setup()
         {
-            _presentationModel = new PresentationModel.PresentationModel();
-            _privatePresentationModel = new PrivateObject(_presentationModel);
+            _mockModel = new Mock<Model>();
+            _presentationModel = new PresentationModel(_mockModel.Object);
+        }
+
+        [TestMethod]
+        public void SetModelStateTest()
+        {
+            _presentationModel.SetModelState(Model.ModelState.Drawing);
+
+            _mockModel.Verify(m => m.SetModelState(Model.ModelState.Drawing), Times.Once);
+        }
+
+        [TestMethod]
+        public void DeleteShapeTest()
+        {
+            _presentationModel.DeleteShape();
+
+            _mockModel.Verify(m => m.RemoveShape(), Times.Once);
+        }
+
+        [TestMethod]
+        public void PressedPointerTest()
+        {
+            var point = new Point(1, 1);
+
+            _presentationModel.PressedPointer(point);
+
+            _mockModel.Verify(m => m.MouseDown(point, It.IsAny<ShapeType>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void MovedPointerTest()
+        {
+            var point = new Point(1, 1);
+
+            _presentationModel.MovedPointer(point);
+
+            _mockModel.Verify(m => m.MouseMove(point), Times.Once);
+        }
+
+        [TestMethod]
+        public void ReleasedPointerTest()
+        {
+            var point = new Point(1, 1);
+
+            _presentationModel.ReleasedPointer(point);
+
+            _mockModel.Verify(m => m.MouseUp(point, It.IsAny<ShapeType>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            _presentationModel.Clear();
+
+            _mockModel.Verify(m => m.Clear(), Times.Once);
+        }
+
+        [TestMethod]
+        public void RemoveShapeTest()
+        {
+            _presentationModel.RemoveShape(1);
+
+            _mockModel.Verify(m => m.RemoveShapeByIndex(1), Times.Once);
+        }
+
+        [TestMethod]
+        public void InsertShapeTest()
+        {
+            _presentationModel.InsertShape(ShapeType.LINE);
+
+            _mockModel.Verify(m => m.InsertShape(ShapeType.LINE), Times.Once);
+        }
+
+        [TestMethod]
+        public void HandleButtonClickTest()
+        {
+            _presentationModel.HandleButtonClick(0);
+
+            Assert.AreEqual(ShapeType.LINE, _presentationModel.Type);
         }
     }
 }
